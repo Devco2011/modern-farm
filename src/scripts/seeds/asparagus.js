b91901f784cb75cea6b49cb65690dfa96e21aaaf
Devco2011 <devco2011@gmail.com>
2020-08-30 14:09:23 -0500

    Finished sewing the seeds

--- src/scripts/seeds/asparagus.js
+++ src/scripts/seeds/asparagus.js
@@ -1,8 +1,8 @@
 export const createAsparagus = () => {
-    const asparagusObj = {
-     type: "Asparagus",
-     height: 24,
-     output: 4   
+    const asparagus = {
+        type: "Asparagus",
+        height: 24,
+        output: 4
     };
-    return asparagusObj;
+    return asparagus;
 }
