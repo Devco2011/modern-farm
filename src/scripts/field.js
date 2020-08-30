5f9dfeecc24d51962935e6b38c99c4e2d49144e9
Devco2011 <devco2011@gmail.com>
2020-08-30 12:36:11 -0500

    Tilling Complete

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,5 +1,14 @@
-const fieldPlants = []
+/* Define a variable whose initial value is an empty array. 
+This array will store all of the plants that are growing in the field. */
 
-export const addPlant= (sunflowerObject) => {
+const fieldPlants = [];
 
+// This function will add the seed to the field 
+export const addPlant = (seed) => {
+    fieldPlants.push(seed)
+
+}
+
+export const usePlants = () => {
+    return fieldPlants
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,15 @@
-//console.log("Welcome to the main module")
+console.log("Welcome to the main module");
+import { createPotato } from "./seeds/potato.js";
 import { createPlan } from "./plan.js";
+import { addPlant } from "./field.js";
+import { usePlants } from "./field.js";
 
+const potatoSeed = createPotato()
+
+addPlant(potatoSeed);
+const field = usePlants();
+
+console.log(field)
 const yearlyPlan = createPlan()
-
 console.log(yearlyPlan)
 
